5b896ac5794c1b80930589ac892ec5ff58abacd9
zhaENS <barbarazha@hotmail.com>
2015-05-22 19:23:37 +0200

    Histones collision detect finish
    rest :reflection

--- code/HistoneReflectionTest.m
+++ code/HistoneReflectionTest.m
@@ -61,6 +61,8 @@ curPosVertex1 = zeros(numSteps,histoneParams.numHistones);
 curPosVertex2 = zeros(numSteps,histoneParams.numHistones);
 curPosSlope   = zeros(numSteps,histoneParams.numHistones);
 edgeLength    = zeros(numSteps,cp.numBeads-1);
+posHistone    = zeros(numSteps,histoneParams.numHistones);
+curPos        = zeros(histoneParams.numHistones,3,numSteps);
 for sIdx = 1:numSteps
     % advance one simulation step
     r.Step;
@@ -74,68 +76,67 @@ for sIdx = 1:numSteps
     for i = 1:cp.numBeads-1
      edgeLength(sIdx,i) = sqrt(sum((chainPos(i+1,:)-chainPos(i,:)).^2));
     end
-    edgeLengthTotal = [zeros(numSteps,1), cumsum(edgeLength,2)];
+  edgeLengthTotal = [zeros(numSteps,1),cumsum(edgeLength,2)];
     
     %stock the previous position and current postion of histones in each
     %step;
-    curPosition (:,:,sIdx)  = h.curPos;%coordinate in 3D;
-    curPosVertex1(sIdx,:)   =  h.curPosVertex1;
-    curPosVertex2(sIdx,:)   =  h.curPosVertex2;
-    curPosSlope(sIdx,:) = h.curPosSlope;%position ratio;
-    if sIdx >= 2
-      for vIdx = 1:histoneParams.numHistones
-          %the velocity of each histone during deltaT;
-        v(sIdx,vIdx) = norm(curPosition(vIdx,:,sIdx)-curPosition(vIdx,:,sIdx-1))/simulatorParams.simulator.dt;
-      end
-   
+    curPos(:,:,sIdx)        =h.curPos; %the coordinates 3D of histones in each step;
+    curPosVertex1(sIdx,:)   =  h.curPosVertex1;%vertices left
+    curPosVertex2(sIdx,:)   =  h.curPosVertex2;%vertices right 
+    curPosSlope(sIdx,:)     = h.curPosSlope;%position ratio;
+    posHistone(sIdx,:)      = edgeLengthTotal(sIdx,curPosVertex1(sIdx,:))+edgeLength(sIdx,curPosVertex1(sIdx,:))...
+                            .*curPosSlope(sIdx,:);%the position 1D of histones in each step;
+    if sIdx >1
+    %the velocity of each histone during deltaT;
+        v(sIdx,:) = (posHistone(sIdx,:)-posHistone(sIdx-1,:))/simulatorParams.simulator.dt;
     %detect collision
-    cIdx = 1;
-    k = zeros(histoneParams.numHistones);
+    
+   
    for vIdx = 1:histoneParams.numHistones
        for kIdx = (vIdx+1):histoneParams.numHistones
-           if min(curPosVertex1(sIdx-1,vIdx)-1,curPosVertex1(sIdx-1,kIdx)-1)~=0
-         Dist   = edgeLengthTotal(sIdx-1,max(curPosVertex1(sIdx-1,vIdx),curPosVertex1(sIdx-1,kIdx)))...
-                 -edgeLengthTotal(sIdx-1,min(curPosVertex1(sIdx-1,vIdx)-1,curPosVertex1(sIdx-1,kIdx)-1));
-           else
-         Dist   = edgeLengthTotal(sIdx-1,max(curPosVertex1(sIdx-1,vIdx),curPosVertex1(sIdx-1,kIdx)));      
-           end
-         if curPosVertex1(sIdx-1,vIdx)>curPosVertex1(sIdx-1,kIdx)
-             k(vIdx,kIdx) = (Dist-edgeLength(sIdx-1,kIdx)*curPosSlope(sIdx-1,kIdx)-edgeLength(sIdx-1,vIdx)*...
-             (1-curPosSlope(sIdx-1,vIdx)))/(v(sIdx,vIdx)-v(sIdx,kIdx));
-         else
-             k(vIdx,kIdx) = (Dist-edgeLength(sIdx-1,vIdx)*curPosSlope(sIdx-1,vIdx)-edgeLength(sIdx-1,kIdx)*...
-             (1-curPosSlope(sIdx-1,kIdx)))/(v(sIdx,vIdx)-v(sIdx,kIdx));
-
-         end
-   %k=(x2(t)-x1(t))/(v1(t+deltaT)-v2(t+deltaT))
-       if (k(vIdx,kIdx) < simulatorParams.simulator.dt) &&(k(vIdx,kIdx)>0)
-            sprintf('point %s and point %s are colliding ',num2str(vIdx),num2str(kIdx))
-        end
-        cIdx = cIdx+1; 
-       end
-   end
-    end
-% find colliding histones
-        if sIdx >1
-            [hist1,hist2] = find(k>0 & k<=simulatorParams.simulator.dt);
+           
+           k(vIdx,kIdx)  = (posHistone(sIdx-1,vIdx)-posHistone(sIdx-1,kIdx))/(v(sIdx,vIdx)-v(sIdx,kIdx));
+               % find colliding histones
+            [hist1,hist2] = find(k(vIdx,kIdx)>0 & k(vIdx,kIdx)<=simulatorParams.simulator.dt);
             if ~isempty(hist1)
                 disp('collision')
+             %transforme the coordinate of particles collision to 3D;
+            curPos(vIdx,:,sIdx-1) =  (posHistone(sIdx-1,vIdx)-edgeLengthTotal(sIdx-1,curPosVertex1(sIdx-1,vIdx)))...
+                                      /edgeLength(sIdx-1,curPosVertex1(sIdx,vIdx))...
+                                      *(chainPos(curPosVertex2(sIdx-1,vIdx),:)-chainPos(curPosVertex1(sIdx-1,vIdx),:))...
+                                      +chainPos(curPosVertex1(sIdx-1,vIdx),:);
+            curPos(kIdx,:,sIdx-1) =  (posHistone(sIdx-1,kIdx)-edgeLengthTotal(sIdx-1,curPosVertex1(sIdx-1,kIdx)))...
+                                      /edgeLength(sIdx-1,curPosVertex1(sIdx,kIdx))...
+                                      *(chainPos(curPosVertex2(sIdx-1,kIdx),:)-chainPos(curPosVertex1(sIdx-1,kIdx),:))...
+                                      +chainPos(curPosVertex1(sIdx-1,kIdx),:);
+             %reflection
+             
             end
-        end
+       end
+   end
+   %k=(x2(t)-x1(t))/(v1(t+deltaT)-v2(t+deltaT))
+    % find colliding histones
+%             [hist1,hist2] = find(k>0 & k<=simulatorParams.simulator.dt);
+%             if ~isempty(hist1)
+%                 disp('collision')
+%             end
+    end
         
 %   update histone graphics    
+
     if simulatorParams.simulator.showSimulation
           if sIdx >1
          delete(b(1:histoneParams.numHistones));
          end
 
-        set(histHandle,'XData',h.curPos(:,1),'YData',h.curPos(:,2),'ZData',h.curPos(:,3));
-        text(h.curPos(beads,1)+0.001,h.curPos(beads,2)-0.01,h.curPos(beads,3)+0.002,num2str(beads'),'FontSize',15);
-      
+       % set(histHandle,'XData',h.curPos(:,1),'YData',h.curPos(:,2),'ZData',h.curPos(:,3));
+        %text(h.curPos(beads,1)+0.001,h.curPos(beads,2)-0.01,h.curPos(beads,3)+0.002,num2str(beads'),'FontSize',15);
+        set(histHandle,'XData',curPos(:,1,sIdx),'YData',curPos(:,2,sIdx),'ZData',curPos(:,3,sIdx));
+        text(curPos(beads,1,sIdx)+0.001,curPos(beads,2,sIdx)-0.01,curPos(beads,3,sIdx)+0.002,num2str(beads'),'FontSize',15);
+        b = findobj(gca,'Type','Text');
        
         drawnow
     end
-  b = findobj(gca,'Type','Text');
+end
 end
  
-end
