9d5779631d5a39436ce041c37a6744ee5b14e468
zhaENS <barbarazha@hotmail.com>
2015-07-13 09:58:16 +0800

    play with parameters

--- code/startyou.m
+++ code/startyou.m
@@ -3,5 +3,7 @@
 dbstop if error
 curPath = pwd;   
 addpath(genpath(pwd));
-addpath(genpath(fullfile(curPath,'..','..','PolymerChainDynamics','Code')));
-addpath(genpath(fullfile(curPath,'..','..','Utils')));
+%addpath(genpath(fullfile(curPath,'..','..','PolymerChainDynamics','Code')));
+%addpath(genpath(fullfile(curPath,'..','..','Utils')));
+addpath(genpath(fullfile(curPath,'..','..','Code')));
+addpath(genpath(fullfile(curPath,'..','..','..','Utils')));
