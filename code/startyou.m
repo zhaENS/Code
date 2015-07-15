90d97e1bbe3db25222fba2895b2cb5ee65ed24d0
unknown <barbarazha@hotmail.com>
2015-07-15 22:57:30 +0800

    commit

--- code/startyou.m
+++ code/startyou.m
@@ -5,5 +5,5 @@ curPath = pwd;
 addpath(genpath(pwd));
 addpath(genpath(fullfile(curPath,'..','..','PolymerChainDynamics','Code')));
 addpath(genpath(fullfile(curPath,'..','..','Utils')));
-%addpath(genpath(fullfile(curPath,'..','..','Code')));
-%addpath(genpath(fullfile(curPath,'..','..','..','Utils')));
+addpath(genpath(fullfile(curPath,'..','..','Code')));
+addpath(genpath(fullfile(curPath,'..','..','..','Utils')));
