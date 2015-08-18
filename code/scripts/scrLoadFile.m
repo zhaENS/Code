7493a36250a55bf8847beed9551b8205dc1ac033
zhaENS <barbarazha@hotmail.com>
2015-08-18 18:23:07 +0200

    commit

--- code/scripts/scrLoadFile.m
+++ code/scripts/scrLoadFile.m
@@ -1,5 +1,6 @@
 clear all
-file=dir('C:\projectsENS\PolymerChainDynamicsResults\MSD64beadsMoveOntheBoundary\*.mat');
+%file=dir('C:\projectsENS\PolymerChainDynamicsResults\MSD64beadsMoveOntheBoundary\*.mat');
+file=dir('D:\Zha\Project\PolymerChainDynamicsResults\18_8_2015\*.mat');
 m=zeros(64,5000,numel(file));
 for i=1:length(file)
 load(file(i).name);
