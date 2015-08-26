d26f1617f179735b5ad7604a5e4b48756697bdee
zhaENS <barbarazha@hotmail.com>
2015-08-26 17:45:23 +0200

    add new properties to do oscillatory motion

--- code/DiffusionOnSphere.m
+++ code/DiffusionOnSphere.m
@@ -5,19 +5,19 @@
 %domainCenter : the center of mass of sphere
 %diffConst : a diffusion const for the processus 
 %radius : the radius of the sphere
-function paths = DiffusionOnSphere(initialPoint,dt,diffConst,numSteps,domainCenter,radius,w)
+function paths = DiffusionOnSphere(initialPoint,dt,diffConst,numSteps,domainCenter,radius,ang,magnitude)
 %check initialPoint is on the sphere or not
 rho = sqrt(sum((initialPoint-domainCenter).^2,2));
 if (rho-radius)^2>eps
     error('the initial point is not on the sphere');
 end
 %generate a path 2D with numStep;
-if w==0
-noise = [zeros(1,2);sqrt(2*diffConst*dt)*randn(numSteps-1,2)];
+if all([ang' magnitude])~=0
+noise = [zeros(1,2);bsxfun(@plus,sqrt(2*diffConst*dt)*randn(numSteps-1,2),magnitude.*[cos(ang),sin(ang)])];
 else
-noise=[zeros(1,2);sqrt(2*diffConst*dt)*ones(numSteps-1,2)*cos(w*dt)];    
+noise = [zeros(1,2);sqrt(2*diffConst*dt)*randn(numSteps-1,2)];    
 end
-paths =  cumsum(noise);
+paths = cumsum(noise);
 
 %find the phi and theta on the sphere;
 phi = atan(initialPoint(2)/initialPoint(1));
@@ -35,16 +35,14 @@ paths(:,3) = bsxfun(@plus, paths(:,3),radius);
 % plot3([domainCenter,initialPoint(1,1)],[domainCenter initialPoint(1,2)],[domainCenter initialPoint(1,3)],'g','LineWidth',3);hold on
 % plot3([paths(:,1)],[paths(:,2)],[paths(:,3)],'g','LineWidth',3);
 
-%hold on
-%defini the rotation matrix; 
+% hold on
+% % %defini the rotation matrix; 
 % plot3(initialPoint(:,1),initialPoint(:,2),initialPoint(:,3),'o','MarkerSize',4);
 % hold on;
 Ry = [cos(theta) 0 sin(theta) ;0 1 0 ;-sin(theta) 0 cos(theta)];
 Rz = [cos(phi) -sin(phi) 0;sin(phi) cos(phi) 0;0 0 1];
 
 paths = (Rz*Ry*bsxfun(@minus,paths,domainCenter)')';
-%paths = bsxfun(@plus, paths,initialPoint);
-%paths(1,:) =initialPoint;
 % c=[rand rand rand];
 % [sx,sy,sz]  = sphere(20);
 % sx          = sx*radius;
@@ -54,7 +52,7 @@ paths = (Rz*Ry*bsxfun(@minus,paths,domainCenter)')';
 % cameratoolbar
 % daspect([1 1 1])
 % plot3(paths(:,1), paths(:,2),paths(:,3),'LineWidth',2);
-% 
+
 
 %project the paths back on the sphere;
 distToCenter = pdist2mex(paths(2:end,:)',domainCenter','euc',[],[],[]);
--- code/scripts/scrDiffusionOnSphere.m
+++ code/scripts/scrDiffusionOnSphere.m
@@ -8,5 +8,5 @@ diffConst    = 0.1;
 numSteps     = 100;
 domainCenter = [0 0 0];
 radius       = sqrt(sum(initialPoint.^2));
-w =0;
+w =5;
 paths = DiffusionOnSphere(initialPoint,dt,diffConst,numSteps,domainCenter,radius,w);
